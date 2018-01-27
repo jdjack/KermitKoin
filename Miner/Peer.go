b6f0c07ba15a83fb47522416059afa3694ccb595
Thomas Pointon <thomas.pointon16@imperial.ac.uk>
2018-01-27 16:43:40 +0000

    Fetching currently live peers from always on peers

--- Miner/Peer.go
+++ Miner/Peer.go
@@ -2,39 +2,40 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
-type IP string
-
 type Peer struct {
-	IPAddr IP   `json:"IP"`
-	Alive  bool `json:"-"`
+	IP string `json:"IP"`
 }
 
-var knownPeers []Peer
+// Peers which are always alive
+var alwaysOnPeers []Peer
+
+// Peers which are currently alive
+var livePeers []Peer
 
 // Send all the peers that are alive
 func GetPeers(w http.ResponseWriter, r *http.Request) {
 
-	// Filter peers that are alive
-	var livePeers []Peer
-	for _, peer := range peers {
-		if peer.Alive {
-			append(livePeers, peer)
-		}
-	}
-
 	// Encode the response and send it
-	w.write(json.Marshal(peers))
+	encoded, err := json.Marshal(livePeers)
+	if err != nil {
+		w.Write(encoded)
+	} else {
+		log.Fatal(err)
+	}
 
 }
 
-// Loads the current known peers from a file
-func GetKnownPeers() []IP {
+// Loads the always on peers from a file
+func GetAlwaysOnPeers() []Peer {
 
 	// Peers are stored as a list of IP's on each line of a file
 	file, err := os.Open("/peers.txt")
@@ -44,16 +45,46 @@ func GetKnownPeers() []IP {
 	defer file.Close()
 
 	// Read each peer into the peers array
-	var knownPeers []IP
+	var peersFromDisk []Peer
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		append(knownPeers, scanner.Text())
+		peersFromDisk = append(peersFromDisk, Peer{scanner.Text()})
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	return knownPeers
+	return peersFromDisk
 
+}
+
+// Fetch the live peers from a known peer
+func FetchLivePeers() []Peer {
+
+	rand.Seed(time.Now().UnixNano())
+
+	// Choose a random always-on peer to connect to
+	randAlwaysOnPeer := rand.Intn(len(alwaysOnPeers) - 1)
+	randKnownIP := alwaysOnPeers[randAlwaysOnPeer].IP
+	r, err := netClient.Get("http://" + randKnownIP + "/getPeers")
+	if err != nil {
+
+		// Remove this peer from the list
+		alwaysOnPeers = append(
+			alwaysOnPeers[:randAlwaysOnPeer],
+			alwaysOnPeers[randAlwaysOnPeer+1:]...)
+
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	// Read and parse the response
+	buf := bytes.NewBuffer(make([]byte, 0, r.ContentLength))
+	_, readErr := buf.ReadFrom(r.Body)
+	body := buf.Bytes()
+	livePeers := make([]Peer, 0)
+	json.Unmarshal(body, &livePeers)
+
+	return livePeers
 }
