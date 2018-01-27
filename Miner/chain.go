8ac9ac4f2e934987b679d20ba114032b19173a82
Unknown <sjm116@ic.ac.uk>
2018-01-27 14:21:46 +0000

    Implement chain hash getter

--- Miner/chain.go
+++ Miner/chain.go
@@ -4,15 +4,26 @@ type Chain struct {
   chain []Block
 }
 
-func (chain *Chain) isLonger(chain2 *Chain) bool {
+func (chain *Chain) IsLonger(chain2 *Chain) bool {
   return len(chain.chain) > len(chain2.chain)
 }
 
-func (chain *Chain) getBlockByIndex(index int) *Block {
+func (chain *Chain) GetBlockByIndex(index int) *Block {
   if index < 0 || index > len(chain.chain) {
     return nil
   } else {
     return &chain.chain[index]
   }
+}
+
+func (chain *Chain) GetBlockByHash(hash string) *Block {
+  var retBlock Block
+  for _, block := range chain.chain {
+    if block.hash == hash {
+      retBlock = block
+      return &retBlock
+    }
+  }
+  return nil
 }
 
