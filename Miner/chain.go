6676cc83125816350b3710b09f6f83f64dd2f5d2
Unknown <sjm116@ic.ac.uk>
2018-01-27 14:46:45 +0000

    Implement chain validator

--- Miner/chain.go
+++ Miner/chain.go
@@ -4,11 +4,11 @@ type Chain struct {
   chain []Block
 }
 
-func (chain *Chain) IsLonger(chain2 *Chain) bool {
+func (chain *Chain) isLonger(chain2 *Chain) bool {
   return len(chain.chain) > len(chain2.chain)
 }
 
-func (chain *Chain) GetBlockByIndex(index int) *Block {
+func (chain *Chain) getBlockByIndex(index int) *Block {
   if index < 0 || index > len(chain.chain) {
     return nil
   } else {
@@ -16,7 +16,7 @@ func (chain *Chain) GetBlockByIndex(index int) *Block {
   }
 }
 
-func (chain *Chain) GetBlockByHash(hash string) *Block {
+func (chain *Chain) getBlockByHash(hash string) *Block {
   var retBlock Block
   for _, block := range chain.chain {
     if block.hash == hash {
@@ -27,3 +27,26 @@ func (chain *Chain) GetBlockByHash(hash string) *Block {
   return nil
 }
 
+func (chain *Chain) validate() bool {
+  for index, block := range chain.chain {
+    // Check blocks are valid.
+    if !block.validate() {
+      return false
+    }
+    // Check blocks are in order.
+    if block.index != index {
+      return false
+    }
+    // Check all prev_hash values are correct.
+    if index > 0 {
+      if block.prev_hash != (chain.chain[index - 1]).hash {
+        return false
+      }
+    }
+    // Check genesis block exists
+    if len(chain.chain) == 0 {
+      return false
+    }
+  }
+  return true
+}
