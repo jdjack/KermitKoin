8b73360b79ee95b4318bfd6685cef3da7223aa53
Liam Pilot <lp616@ic.ac.uk>
2018-01-27 13:16:41 +0000

    block hash function

--- Miner/block.go
+++ Miner/block.go
@@ -1,9 +1,10 @@
 package main
 
+import "crypto/sha256"
 
 type Block struct {
   index int
-  prev_hash int
+  prev_hash string
   git_hash string
   repo_id string
   timestamp string
@@ -16,7 +17,9 @@ func (block *Block) verify_transaction() bool {
 }
 
 func (block *Block) generate_hash() string {
-  return nil
+  h := sha256.New()
+  h.Write([]byte(block.prev_hash + string(block.index) + block.timestamp + block.git_hash))
+  return string(h.Sum(nil))
 }
 
 func (block *Block) validate() bool {
