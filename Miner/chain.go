8eea20e7fc6b60ca0cdf8a717ce7cd782181d9dd
Unknown <sjm116@ic.ac.uk>
2018-01-27 14:06:42 +0000

    Implement chain index getter

--- Miner/chain.go
+++ Miner/chain.go
@@ -4,6 +4,15 @@ type Chain struct {
   chain []Block
 }
 
-func (chain1 *Chain) isLonger(chain2 *Chain) bool {
-  return len(chain1.chain) > len(chain2.chain)
+func (chain *Chain) isLonger(chain2 *Chain) bool {
+  return len(chain.chain) > len(chain2.chain)
 }
+
+func (chain *Chain) getBlockByIndex(index int) *Block {
+  if index < 0 || index > len(chain.chain) {
+    return nil
+  } else {
+    return &chain.chain[index]
+  }
+}
+
